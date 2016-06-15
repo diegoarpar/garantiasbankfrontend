932c75b2109ee4e04938b15be4b22f8d5eadf3a6
dapadillar <hellomotorola>
2016-06-15 00:16:41 -0500

    fields administration

---
+++ assets/app/entities/garantias/campos/genericos/controller/administrador-camposgenericos.controller.js
@@ -0,0 +1,45 @@
+/**
+ * Created by joag on 9/06/16.
+ */
+(function(){
+   'use strict';
+    angular.module("wpc")
+            .controller('CamposGenericosController', AdministradorUsuariosController);
+
+    AdministradorUsuariosController.$inject = ['$scope', 'AuthenticationGetUserServices','AuthenticationGetRolesServices','AuthenticationGetPermissionServices', '$location','$rootScope','$window','$route'];
+
+    function AdministradorUsuariosController($scope, AuthenticationGetUserServices,AuthenticationGetRolesServices,AuthenticationGetPermissionServices, $location,$rootScope,$window,$route) {
+        $scope.users=AuthenticationGetUserServices.show();
+        $scope.roles=AuthenticationGetRolesServices.show();
+        $scope.permission=[];
+        $scope.selectedUser={};
+        $scope.selectedRole={};
+        $scope.addUser=function(){
+            var user={user:$scope.nombre, pass:$scope.contrasena, completeName:$scope.nombreCompleto};
+
+
+            AuthenticationGetUserServices.create({user:$scope.nombre, pass:sha256($scope.contrasena), completeName:$scope.nombreCompleto})
+                .$promise.then(function(){
+                    $scope.users=AuthenticationGetUserServices.show();
+                });
+            
+        };
+        $scope.editPermission=function(c){
+                    $scope.selectedUser=c;
+                    $scope.permission=AuthenticationGetPermissionServices.show({user:$scope.selectedUser.user});
+        };
+        $scope.editInformation=function(c){
+                    $scope.selectedUser=c;
+
+        };
+        $scope.removeUser=function(idx){
+                    $scope.users.splice(idx, 1);
+        };
+        $scope.saveRole=function(){
+                        var permission={user:$scope.selectedUser.user,roleName:$scope.selectedRole};
+                        AuthenticationGetPermissionServices.create({user:$scope.selectedUser.user,roleName:$scope.selectedRole});
+                         $scope.permission=AuthenticationGetPermissionServices.show({user:$scope.selectedUser.user});
+        };
+    }
+    }
+)();

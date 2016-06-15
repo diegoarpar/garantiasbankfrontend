1c7f2b8005bd85ef290f69ff53b4f584b4545c17
dapadillar <hellomotorola>
2016-06-14 23:27:42 -0500

    look and feel

--- assets/app/navbar/nav-bar.controller.js
+++ assets/app/navbar/nav-bar.controller.js
@@ -9,7 +9,12 @@
         NavBarController.$inject =  ['$scope', 'ProductServices', '$location','$uibModal'];
 
         function NavBarController($scope, ProductServices, $location,$uibModal) {
+            $scope.toggleStyle= function(){
+                $scope.bodyCon = !$scope.bodyCon;
+                $scope.noneStyle = !$scope.noneStyle;
+                $scope.menu=!$scope.menu;
 
+            }
             $scope.open = function (size) {
                 var modalInstance = $uibModal.open({
                         animation: $scope.animationsEnabled,
